dd0fe67eb62b548f5348a837fdc72fdf08998779
Matt Luna <mat.c.luna@gmail.com>
2021-07-19 18:29:36 -0500

    Can't get validation to pass information

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // Assignment code here
 
-//Password Prompt
-var userCharAmt = window.prompt("How long does your password need to be?")
 
 //Restrict Password to Numbers Only.
 //How to I set a value to exit if this is true? Do I wrap it all in an if statement?
@@ -14,36 +12,75 @@ var capitalAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
 var lowerAlpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'z', 'y', 'z'];
 var specialChar = ['!', '@', '#', '$', '&'];
 var genNumb = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-let allChar = [capitalAlpha, lowerAlpha, specialChar, genNumb];
+let allChar = [];
 
 var passWord = [];
+var userCharAmt;
 
 console.log(allChar);
 
-//label of test
-console.log("All Characters Length of Capital Alpha");
-//length test on allChar at position 0
-console.log(allChar[0].length);
+// //label of test
+// console.log("All Characters Length of Capital Alpha");
+// //length test on allChar at position 0
+// console.log(allChar[0].length);
 
-//This returns all of the keys within the object. How do I get values? 
-console.log(allChar[0][0]);
+// //This returns all of the keys within the object. How do I get values? 
+// console.log(allChar[0][0]);
 
-var i = Math.floor(Math.random() * 4);
-//Label
-console.log("Return the Array of i");
-console.log(allChar[i])
-console.log("Return the Length of i");
-console.log(allChar[i].length);
-var k = allChar[i].length
+// var i = Math.floor(Math.random() * 4);
+// //Label
+// console.log("Return the Array of i");
+// console.log(allChar[i])
+// console.log("Return the Length of i");
+// console.log(allChar[i].length);
+// var k = allChar[i].length
 
-var j = Math.floor(Math.random() * k)
-//Value of j
-console.log("Value of j");
-console.log(j);
+// var j = Math.floor(Math.random() * k)
+// //Value of j
+// console.log("Value of j");
+// console.log(j);
 
-//Label
-console.log("All Characters Passed with Second");
-console.log(allChar[i][j]);
+// //Label
+// console.log("All Characters Passed with Second");
+// console.log(allChar[i][j]);
+
+//Prompt
+function questions() {
+  var question1 = window.prompt("What is your mother's maiden name?");
+  var question2 = window.prompt("What is the name of the street you grew up on?");
+  var question3 = window.prompt("What year did you graduate highschool?");
+  var userCharAmt = window.prompt("Select a password length between 8 and 128 characters");
+  if (Number.isInteger(userCharAmt) && (userCharAmt >= 8 && userCharAmt <= 128)) {
+    return true;
+  } else {
+    window.prompt("Please select password length between 8 and 128 characters")
+    return false;
+  }
+
+};
+
+function includeChars() {
+  var capitalLets = window.confirm("Should capital letters be included in your password?")
+  if(capitalLets){
+    allChar.push(capitalAlpha)
+  };
+  var lowerLets = window.confirm("Should lowercase letters be included in your password?")
+  if (lowerLets) {
+    allChar.push(lowerAlpha)
+  };
+  var specials = window.confirm("Should special characters be included in your password?")
+  if (specials) {
+    allChar.push(specialChar)
+  };
+  var numbnumbs = window.confirm("Should numbers be included in your password?")
+  if (numbnumbs) {
+    allChar.push(genNumb)
+  };
+}
+
+questions();
+includeChars();
+console.log(allChar);
 
 
 //Multi-dimensional array
@@ -76,9 +113,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  console.log("in writePassword")
   generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = passWord;
+  var password1 = passWord;
+  password1 = document.querySelector("#password");
 };
 
 // Add event listener to generate button
