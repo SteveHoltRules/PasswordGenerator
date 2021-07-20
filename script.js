b32cea45780daa4a25432f20990b857c4872ae5e
Matt Luna <mat.c.luna@gmail.com>
2021-07-19 22:23:53 -0500

    working baseline

--- script.js
+++ script.js
@@ -19,36 +19,12 @@ var userCharAmt;
 
 console.log(allChar);
 
-// //label of test
-// console.log("All Characters Length of Capital Alpha");
-// //length test on allChar at position 0
-// console.log(allChar[0].length);
-
-// //This returns all of the keys within the object. How do I get values? 
-// console.log(allChar[0][0]);
-
-// var i = Math.floor(Math.random() * 4);
-// //Label
-// console.log("Return the Array of i");
-// console.log(allChar[i])
-// console.log("Return the Length of i");
-// console.log(allChar[i].length);
-// var k = allChar[i].length
-
-// var j = Math.floor(Math.random() * k)
-// //Value of j
-// console.log("Value of j");
-// console.log(j);
-
-// //Label
-// console.log("All Characters Passed with Second");
-// console.log(allChar[i][j]);
-
 //Prompt
 var userCharAmt;
 
+userCharAmt = window.prompt("Select a password length between 8 and 128 characters");
+
 function questions() {
-  userCharAmt = window.prompt("Select a password length between 8 and 128 characters");
   if (Number.isInteger(userCharAmt) && (userCharAmt >= 8 && userCharAmt <= 128)) {
     return true;
   } else {
@@ -76,45 +52,41 @@ function includeChars() {
   };
 }
 
-questions();
-includeChars();
-console.log(allChar);
-
-
 //Multi-dimensional array
 function generatePassword() {
+  console.log("in generate password");
+  console.log("User Character Amount : " + userCharAmt);
+  questions();
+  includeChars();
   passWord = [];
   for (var i = 0; i<userCharAmt; i++) {
     var first = Math.floor(Math.random() * 4)
     console.log("First Array Value")
     console.log(first)
-    var arrLength = allChar[first].length
-    console.log("First Array Value")
+    var arrLength = (allChar[first].length)
+    console.log("Array Length")
     console.log(arrLength)
     var second = Math.floor(Math.random() * arrLength)
-    console.log("Third Array Value")
+    console.log("Second Value")
     console.log(second)
     console.log("Password Value")
     console.log(allChar[first][second]);
     console.log(i);
     var j = allChar[first][second];
     passWord.push(j);
-  }
+  };
   passWord = passWord.join('').trim();
 };
-
-//Show Generated Password
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  console.log("in writePassword")
+  console.log("in writePassword");
   generatePassword();
-  var password1 = passWord;
-  password1 = document.querySelector("#password");
+  var password = passWord;
+  password = document.querySelector("#password");
 };
 
 // Add event listener to generate button
