cf3e5688e1489132093e9b87f72438f60603d872
Matt Luna <mat.c.luna@gmail.com>
2021-07-19 23:25:46 -0500

    Password Generator

--- script.js
+++ script.js
@@ -1,38 +1,27 @@
 // Assignment code here
 
-
-//Restrict Password to Numbers Only.
-//How to I set a value to exit if this is true? Do I wrap it all in an if statement?
-// if (Number.isInteger(userCharAmt) === false) {
-//   userCharAmt = window.prompt("Please use numbers to assign the password length")
-// };
-
 //List of Arrays
 var capitalAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowerAlpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'z', 'y', 'z'];
 var specialChar = ['!', '@', '#', '$', '&'];
 var genNumb = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let allChar = [];
-
-var passWord = [];
-var userCharAmt;
-
-console.log(allChar);
+let passWord = [];
 
 //Prompt
-var userCharAmt;
-
-userCharAmt = window.prompt("Select a password length between 8 and 128 characters");
-
+// Verify the content, convert the prompt to a number
 function questions() {
-  if (Number.isInteger(userCharAmt) && (userCharAmt >= 8 && userCharAmt <= 128)) {
-    return true;
+  var userPrompt = window.prompt("Select a password length between 8 and 128 characters");
+  var userChars = parseInt(userPrompt);
+  if (Number.isInteger(userChars) && (userChars >= 8 && userChars <= 128)) {
+    return userChars;
   } else {
     window.prompt("Please select password length between 8 and 128 characters")
     return false;
   }
 };
 
+//Characters to include | Selected through prompts
 function includeChars() {
   var capitalLets = window.confirm("Should capital letters be included in your password?")
   if(capitalLets){
@@ -52,13 +41,13 @@ function includeChars() {
   };
 }
 
-//Multi-dimensional array
+//Multi-dimensional array to assign the characters
 function generatePassword() {
   console.log("in generate password");
+  var userCharAmt = questions();
   console.log("User Character Amount : " + userCharAmt);
   includeChars();
   console.log(allChar);
-  passWord = [];
   for (var i = 0; i<userCharAmt; i++) {
     var first = Math.floor(Math.random() * 4);
     console.log("First Array Value");
@@ -84,9 +73,8 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   console.log("in writePassword");
   generatePassword();
-  console.log(passWord);
-  var password = passWord;
-  password = document.querySelector("#password");
+  passwordText = document.querySelector("#password");
+  passwordText.value = passWord;
 };
 
 // Add event listener to generate button
